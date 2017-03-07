008d1ebc9cfa2de9cae530860de97e62a7008e0e
eranharel <harel.eran@gmail.com>
2017-03-07 16:42:06 +0200

    Added sigkill fault injector

---
+++ src/main/java/com/outbrain/gomjabbar/faults/SIGKILLer.java
@@ -0,0 +1,34 @@
+package com.outbrain.gomjabbar.faults;
+
+import com.outbrain.gomjabbar.targets.Target;
+import com.outbrain.ob1k.concurrent.ComposableFuture;
+
+import java.util.Objects;
+
+/**
+ * @author Eran Harel
+ */
+public class SIGKILLer implements FaultInjector {
+
+  private final RundeckCommandExecutor commandExecutor;
+
+  public SIGKILLer(final RundeckCommandExecutor commandExecutor) {
+    this.commandExecutor = Objects.requireNonNull(commandExecutor, "commandExecutor must not be null");
+  }
+
+  @Override
+  public String id() {
+    return getClass().getName();
+  }
+
+  @Override
+  public String description() {
+    return "Brutally kills service instances";
+  }
+
+  @Override
+  public ComposableFuture<String> injectFailure(final Target target) {
+    final RundeckCommand command = new RundeckCommand(target.getHost(), String.format("sudo pkill -9 %s", target.getModule()));
+    return commandExecutor.executeCommandAsync(command);
+  }
+}
