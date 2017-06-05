778d08063be195b400c46d41b644df05b7354d85
eranharel <harel.eran@gmail.com>
2017-06-05 14:45:02 +0300

    Fixed to conform with the docs

--- gomjabbar.sh
+++ gomjabbar.sh
@@ -3,7 +3,7 @@
 main() {
 
   local log4jfile=$(pwd)/log4j.xml
-  export MAVEN_OPTS="-Xmx1024m -Xms1024m -Dlog4j.configuration=file://$log4jfile $GJ_OPTS"
+  export MAVEN_OPTS="-Xmx1024m -Xms1024m -Dlog4j.configuration=file://$log4jfile $GJ_OPTIONS"
   mvn exec:java -Dexec.mainClass="com.outbrain.gomjabbar.GomJabbarServer"
 }
 
